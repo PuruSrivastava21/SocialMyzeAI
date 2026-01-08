dc4dd33061425643ba2b131f1539775257d77a53
Puru Srivastava <120162574+PuruSrivastava21@users.noreply.github.com>
2026-01-08 11:00:41 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { Link } from 'react-router-dom'
+import { Zap } from 'lucide-react'
 
 const Footer = () => {
   return (
     <footer className=" mt-20 px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-gray-500">
     <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500/30 pb-6">
         <div className="md:max-w-96">
-            <img src={assets.logo} alt="logo" />
+            <div className="flex items-center">
+            <Link href="/" className="flex items-center space-x-2">
+              <Zap className="w-8 h-8 text-blue-500" />
+              <span className="text-xl sm:text-2xl font-bold text-blue">
+                SocialMyze AI
+              </span>
+            </Link>
+          </div>
             <p className="mt-6 text-sm">
                 Experience the power of AI with SocialMyze. <br/>Transform your content creation with our suite of premium AI tools. Write articles, generate images, and enhance your workflow.
             </p>
