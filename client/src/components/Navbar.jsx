89721858b907eec7255f786ad23c05d57d9319e2
Puru Srivastava <120162574+PuruSrivastava21@users.noreply.github.com>
2026-01-08 11:01:44 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { assets } from '../assets/assets'
-import { useNavigate } from 'react-router-dom'
-import { ArrowRight } from 'lucide-react'
+import { useNavigate, Link } from 'react-router-dom'
+import { ArrowRight, Zap } from 'lucide-react'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
@@ -12,7 +12,14 @@ const Navbar = () => {
   return (
     <div className='fixed z-5 w-full backdrop-blur-2xl flex 
     justify-between items-center py-3 px-4 sm:px-20 xl:px-32'>
-        <img src={assets.logo} alt="logo" className='cursor-pointer w-32 sm:w-44' onClick={()=>navigate('/')}/>
+        <div className="flex items-center">
+            <Link href="/" className="flex items-center space-x-2">
+              <Zap className="w-8 h-8 text-blue-500" />
+              <span className="text-xl sm:text-2xl font-bold text-blue">
+                SocialMyze AI
+              </span>
+            </Link>
+          </div>
         
         {
           user ? <UserButton/> : (<button onClick={openSignIn} className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'>Get Started <ArrowRight className='w-4 h-4' /></button>)
